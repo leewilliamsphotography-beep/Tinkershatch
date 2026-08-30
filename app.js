95b10004898967f99b8788c8e32ec2d64de7c441
leewilliamsphotography-beep <leewilliamsphotography@gmail.com>
2026-08-30 14:54:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -419,6 +419,13 @@ const FeaturedEventsModule = (function() {
     return { init, loadAdminFeatured };
 })();
 
+    function init() {
+        loadFeatured();
+        const btn = document.getElementById('addFeaturedBtn');
+        if (btn) btn.addEventListener('click', addFeatured);
+    }
+    
+    return { init, loadAdminFeatured };
 document.addEventListener('DOMContentLoaded',()=>{
     LayoutModule.init();SplashModule.init();SideNavModule.init();AccessibilityModule.init();QuickJumpModule.init();TimeModule.init();ToastModule.init();ReadAloudModule.init();AmbientAudioModule.init();SensoryModule.init();BackToTopModule.init();SeasonalModule.init();FaviconModule.init();ThemeModule.init();PaletteModule.init();FontSizeModule.init();DyslexiaModule.init();HapticModule.init();BionicModule.init();NextSectionModule.init();RevealModule.init();MoodModule.init();LightboxModule.init();FooterA11yModule.init();ProgressModule.init();SummerEffectsModule.init();TesterModule.init();DatabaseModule.init();FilmNightModule.init();ParallaxModule.init();EventsModule.init();WilfModule.init();MenuModule.init();CommunityModule.init();EnquiriesModule.init();BriefingModule.init();MaintenanceModule.init();WeatherModule.init();CelebrationModule.init();VibeModule.init();GoldenHourModule.init();FeaturedEventsModule.init();
     
