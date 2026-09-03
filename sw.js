d9ed0264d0b511244fff8fe05bf66d319f17e900
leewilliamsphotography-beep <leewilliamsphotography@gmail.com>
2026-09-03 20:05:28 +0100

    word search update

--- sw.js
+++ sw.js
@@ -60,4 +60,45 @@ self.addEventListener('fetch', (event) => {
     );
   }
   // Cross-origin stuff (Google Fonts, ibb.co images) passes straight through
+const CACHE = 'haven-wordsearch-v1';
+const ASSETS = ['./', 'index.html', 'manifest.json', 'icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => Promise.allSettled(ASSETS.map((a) => c.add(a))))
+      .then(() => self.skipWaiting())
+  );
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request, { ignoreSearch: true }).then((hit) => hit ||
+        fetch(e.request).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        }).catch(() => caches.match('./')))
+    );
+  } else if (/(fonts\.googleapis|fonts\.gstatic)\.com$/.test(url.hostname)) {
+    e.respondWith(
+      caches.match(e.request).then((hit) => hit ||
+        fetch(e.request).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        }))
+    );
+  }
+});}) 
